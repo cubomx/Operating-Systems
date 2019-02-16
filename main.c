582c935766f6d222707ee6aa0308a82855ca7066
Sergio <cheservelram657@gmail.com>
2019-02-16 14:07:22 -0600

    Sudoku project finished
    All verification done. Only the project hasn't verification.

--- main.c
+++ main.c
@@ -1,56 +1,51 @@
-/**
- * gcc -o sudoku main.c -lpthread
- *
- **/
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
-
-#define REVISIONES 27
-char valido = '1';
+#define THREADS 27
+char validSudoku = '1';
 int row = 0;
-void *validoColumna(void *param);
-void *validoFila(void * param);
-void *validoCuadro(void *param);
-void *checaCadaLinea(int start, int end, char* []);
-
+int square = 0;
+void *validColumn(void *param);
+void *validRow(void *param);
+void *validSquare(void *param);
+void gettingInfoFromFile(void * param, FILE * file);
 
 int main(int argc, char *argv[]) {
-    pthread_t tid[27];
-    pthread_attr_t attr[27];
+    pthread_t tid[THREADS];
+    pthread_attr_t attr[THREADS];
     FILE * file;
     if (argc < 2){
         printf("Please send fileName");
         return -1;
     }
-    for(int i = 0; i < 18; i++){
+    for(int i = 0; i < THREADS; i++){
         pthread_attr_init(&attr[i]);
     }
     file = fopen(argv[1], "r");
-    int  sudoku[9][9];
-    int index;
-    for(int k = 0; k < 9; k++){
-        for(int l = 0; l < 9; l++){
-            fscanf(file, "%d", &sudoku[l][k]);
-            index++;
-        }
-    }
+
+    int  sudoku[THREADS/3][THREADS/3];
+    /*Getting all the information of the file */
+    gettingInfoFromFile((void *) sudoku, file);
 
     fclose(file);
     int x = 0;
+    /* Creation of the threads of validation of column */
     for (int x = 0; x < 9; x++)
-        pthread_create(&tid[x], &attr[x], validoColumna, (void *)sudoku[x]);
-
-    for(x = 9; x < 18; x++){
-        pthread_create(&tid[x], &attr[x], validoFila, (void *)sudoku);
+        pthread_create(&tid[x], &attr[x], validColumn, (void *) sudoku[x]);
+    /*Creation of the threads of validation of row*/
+    for(; x < 18; x++){
+        pthread_create(&tid[x], &attr[x], validRow, (void *) sudoku);
     }
-
-    for(int j = 0; j < 18; j++)
+    /*Creation of the threads of validation of squares in Sudoku*/
+    for(; x < THREADS; x++){
+        pthread_create(&tid[x], &attr[x], validSquare, (void *) sudoku);
+    }
+    /*Waiting of all threads to perfomed their task*/
+    for(int j = 0; j < THREADS; j++)
         pthread_join(tid[j], NULL);
-
-    if(valido == '1'){
+    /*Checking if there's was repeated value*/
+    if(validSudoku == '1'){
         printf("Sudoku well done");
     }
     else{
@@ -60,12 +55,47 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void *validoColumna(void  * param) {
+
+void gettingInfoFromFile(void * param, FILE * file){
+    int index;
+    int (*sudoku)[9] = param;
+    for(int k = 0; k < 9; k++){
+        for(int l = 0; l < 9; l++){
+            fscanf(file, "%d", &sudoku[l][k]);
+            index++;
+        }
+    }
+}
+
+/*
+ * Checking all the columns of sudoku
+ */
+
+void *validColumn(void *param) {
     int *nums = (int *) param;
     for (int i = 0; i < 9; i++){
         for (int j = i + 1; j < 9; j++) {
             if (nums[i] == nums[j]) {
-                valido = '0';
+                validSudoku = '0';
+            }
+        }
+    }
+    pthread_exit(0);
+}
+
+/*
+ * Checking all the rows of sudoku
+ */
+
+void *validRow(void *param) {
+    int id = row;
+    row++;
+    int (*nums)[9] = param;
+
+    for (int i = 0; i < 9; i++){
+        for (int j = i+1; j < 9; j++) {
+            if (nums[i][id] == nums[j][id]) {
+                validSudoku = '0';
             }
         }
     }
@@ -73,18 +103,34 @@ void *validoColumna(void  * param) {
     pthread_exit(0);
 }
 
-void *validoFila(void  * param) {
-    int id = row;
-    row++;
-    printf("%d\n", id);
+
+/*
+ * Checking all the squares inside the sudoku
+ */
+
+void * validSquare(void *param){
+    int idSquare = square;
+    square++;
     int (*nums)[9] = param;
-    for (int i = 0; i < 9; i++){
-        for (int j = i + 1; j < 9; j++) {
-            if (nums[i][row] == nums[j][row]) {
-                valido = '0';
+    int x = 6, y;
+
+    if(idSquare < 3) x = 0;
+    else if (idSquare < 6) x = 3;
+
+    y = (idSquare % 3)*3;
+
+    for(int posX = x; posX < x+3; posX++){
+        for(int posY = y; posY < y+3; posY++){
+            for(int i = posX; i < x+3; i++){
+                for(int j = posY; j < y+3; j++){
+                    if(posX != i || posY != j){
+                        if(nums[posX][posY] == nums[i][j]){
+                            validSudoku = '0';
+                        }
+                    }
+                }
             }
         }
     }
-
     pthread_exit(0);
 }
