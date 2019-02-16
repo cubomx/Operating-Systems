f03c353f3a80583d49c70807d0dbb16cd66e9a09
Sergio <cheservelram657@gmail.com>
2019-02-16 11:41:37 -0600

    Start of project
    The program already handles the verification of columns. TO DO: verification in square and rows.

---
+++ cmake-build-debug/CMakeFiles/Sudoku.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sudoku.dir/main.c.o"
+  "Sudoku.pdb"
+  "Sudoku"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Sudoku.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,69 @@
+/**
+ * gcc -o sudoku main.c -lpthread
+ *
+ **/
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+#define REVISIONES 27
+char valido = '1';
+void *validoColumna(void *param);
+void *validoFila(void * param);
+void *validoCuadro(void *param);
+void *checaCadaLinea(int start, int end, char* []);
+
+int main(int argc, char *argv[]) {
+    pthread_t tid[27];
+    pthread_attr_t attr[27];
+    FILE * file;
+    if (argc < 2){
+        printf("Please send fileName");
+        return -1;
+    }
+    for(int i = 0; i < 9; i++){
+        pthread_attr_init(&attr[i]);
+    }
+    file = fopen(argv[1], "r");
+    int  sudoku[9][9];
+    int index;
+    for(int k = 0; k < 9; k++){
+        for(int l = 0; l < 9; l++){
+            fscanf(file, "%d", &sudoku[l][k]);
+            index++;
+        }
+    }
+
+
+
+    fclose(file);
+    for (int x = 0; x < 9; x++)
+        pthread_create(&tid[x], &attr[x], validoColumna, (void *)sudoku[x]);
+
+    for(int j = 0; j < 9; j++)
+        pthread_join(tid[j], NULL);
+
+    if(valido == '1'){
+        printf("Sudoku well done");
+    }
+    else{
+        printf("Bad sudoku");
+    }
+
+    return 0;
+}
+
+void *validoColumna(void  * param) {
+    int *nums = (int *) param;
+    for (int i = 0; i < 9; i++){
+        for (int j = i + 1; j < 9; j++) {
+            if (nums[i] == nums[j]) {
+                valido = '0';
+            }
+        }
+    }
+
+    pthread_exit(0);
+}
